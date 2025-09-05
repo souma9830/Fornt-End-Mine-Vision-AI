9401346e4f4ec10e927975b7a96656883fc9ab39
Sajal Pakira <uniquesajal6@gmail.com>
2025-09-05 15:30:42 +0530

    updation in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import useGsapAnimations from "./hooks/useGsapAnimations";
 
 function App() {
+  useGsapAnimations(); // activate GSAP scroll animations
+
   return (
     <div>
       <Navbar />
